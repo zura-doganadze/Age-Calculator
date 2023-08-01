aefc81172bcd2383629f6f07b8fd0be947fec3da
zura-doganadze <123479528+zura-doganadze@users.noreply.github.com>
2023-08-01 14:31:16 +0400

    Age Calculator with JavaScript

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+'use strict'
+const calculateButton = document.getElementById("calculate-button")
+ 
+let userInput = document.getElementById("date");
+let results = document.getElementById("result")
+ 
+// Maximum date control
+userInput.max = new Date().toISOString().split("T")[0];
+
+calculateButton.addEventListener("click", function() {
+    let birthDate = new Date(userInput.value);
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if(m2 >= m1) {
+        m3 = m2 - m1;
+    }else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+    if(d2 >= d1) {
+        d3 = d2 - d1;
+    }else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if(m3 < 0) {
+        m3 = 11;
+        y3--;
+    }
+    results.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
+})
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
